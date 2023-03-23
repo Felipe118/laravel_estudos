e5fdcddbdcfb0c38fb4c081a4103d5457a4b62da
Felipe118 <lipebezerraa@gmail.com>
2023-03-22 22:09:10 -0300

    feat: instalando o Inertia e telescope

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Jobs\LogsJobs;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/job-log', function () {
+
+    LogsJobs::dispatch('ERROR TESTE');
+    return response()->json(['Message' => 'Sucess Jobs Disparado']);
 });
